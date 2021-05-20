58ff66d14ba9369d460fbca61e280efd3109fab0
Babtis <48028537+babtiss@users.noreply.github.com>
2021-05-20 23:54:20 +0300

    Update 1.py

--- Algo 1 course/4 lab/1.py
+++ Algo 1 course/4 lab/1.py
@@ -3,29 +3,27 @@ push (запись в стек) — операция вставки нового
 delete (снятие со стека) — операция удаления нового элемента. O(1)
 Для стека с n элементами требуется O(n) памяти, так как она нужна лишь для хранения самих элементов.
 '''
-def push(item):
-     global length
-     length +=1
-     stek[length] = item
-          
-def delete():
-     global length
-     a=stek[length]
-     length -=1
-     return a
-          
-f=open('stack.in', 'r')
-t=open('stack.out','w') 
- 
-stek = [0 for i in range(1000001)]
-length = -1
-n=int(f.readline())
-for i in range(n):
-    a=f.readline().split()
-    if a[0]=='+':
-        push(a[1])
-    else:
-        print(delete(), file = t)
- 
- 
-t.close()
+class Stack:
+    def __init__(self):
+        self.values = []
+
+    def push(self, item):
+        self.values.append(item)
+
+    def delete(self):
+        if self.values:
+            return self.values.pop()
+        return None
+def main()          
+     f=open('stack.in', 'r')
+     t=open('stack.out','w') 
+
+     n=int(f.readline())
+     for i in range(n):
+         a=f.readline().split()
+         if a[0]=='+':
+             push(a[1])
+         else:
+             print(delete(), file = t)
+     t.close()
+main()
